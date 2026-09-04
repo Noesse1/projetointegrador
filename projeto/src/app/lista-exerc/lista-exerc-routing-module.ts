b7027a81a5d04558ff05aa2ab8ed5c991fc74172
Noesse1 <272362@unifio.edu.br>
2026-09-04 01:21:42 +0000

    Exerc4

--- projeto/src/app/lista-exerc/lista-exerc-routing-module.ts
+++ projeto/src/app/lista-exerc/lista-exerc-routing-module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { Exerc1 } from '../ListaExerc/exerc1/exerc1';
 import { Exerc2 } from './exerc2/exerc2';
 import { Exerc3 } from './exerc3/exerc3';
+import { Exerc4 } from './exerc4/exerc4';
 
 const routes: Routes = [
   { path: 'exerc1', component: Exerc1 }, 
   { path: 'exerc2', component: Exerc2 },
-  { path: 'exerc3', component: Exerc3 }
+  { path: 'exerc3', component: Exerc3 },
+  { path: 'exerc4', component: Exerc4 }
 ];
 
 @NgModule({
